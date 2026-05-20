pumPCin/Unexpected-Keyboard

commit 958900ca2feedee35a5b32c831f505b88628de22

Update target SDK version to 36 (#1190)
* Fix keyboard not showing on Android 16

* Update target SDK version to 36

srcs/juloo.keyboard2/Keyboard2.java
@@ -351,6 +351,15 @@ public class Keyboard2 extends InputMethodService
     return false;
   }
 
+  @Override
+  public boolean onEvaluateInputViewShown()
+  {
+    super.onEvaluateInputViewShown();
+    // Return true regardless of the super call result to fix the keyboard not
+    // being visible on Android 16
+    return true;
+  }
+
   /** Called from [onClick] attributes. */
   public void launch_dictionaries_activity(View v)
   {

build.gradle.kts
@@ -14,12 +14,12 @@ dependencies {
 
 android {
   namespace = "juloo.keyboard2"
-  compileSdkVersion = "android-35"
+  compileSdkVersion = "android-36"
 
   defaultConfig {
     applicationId = "juloo.keyboard2"
     minSdk = 21
-    targetSdk { version = release(35) }
+    targetSdk { version = release(36) }
     versionCode = 50
     versionName = "1.32.1"
   }

commit 7e19b1f68e17edbc39e1cd2e77e3e8566ff6bcb7

Hide keyboard when physical keyboard is connected (#1295)
This is broken since 958900ca

srcs/juloo.keyboard2/Keyboard2.java
@@ -3,6 +3,7 @@ package juloo.keyboard2;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.inputmethodservice.InputMethodService;
 import android.os.Build.VERSION;
 import android.os.Handler;
@@ -361,9 +362,15 @@ public class Keyboard2 extends InputMethodService
   @Override
   public boolean onEvaluateInputViewShown()
   {
-    super.onEvaluateInputViewShown();
-    // Return true regardless of the super call result to fix the keyboard not
-    // being visible on Android 16
+    // Since Android 16, this method returns [false] for unknown reasons.
+    if (super.onEvaluateInputViewShown())
+      return true;
+    if (getResources().getConfiguration().hardKeyboardHidden
+        == Configuration.HARDKEYBOARDHIDDEN_NO)
+    {
+      Logs.debug("Physical keyboard is present");
+      return false;
+    }
     return true;
   }
 